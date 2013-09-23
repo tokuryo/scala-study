5a90d87d6aa6c75d591595ac36624a1c1633deb8
tokuryo <tokuryo@gmail.com>
2013-09-23 19:39:51 +0900

    コンパイルの警告等

--- src/study001/MainMatch.scala
+++ src/study001/MainMatch.scala
@@ -2,11 +2,12 @@ package study001
 
 object MainMatch {
   def main(args: Array[String]): Unit = {
-    var n: Int = 1
+    val n: Int = 1
     // Javaのswitchと違い、一致したらbreakされる。"に" は出力されない
     n match {
       case 1 => println("いち")
-      case 1 | 2 => println("に")
+//        到達しえない
+//      case 1 | 2 => println("に")
       case _ => println("その他")
     }
 
@@ -21,15 +22,16 @@ object MainMatch {
       case n:Int => println("いんと")
     }
     
-    var s0:String = "ほげ"
+    val s0:String = "ほげ"
     s0 match {
       case "ほげ" => println("hoge")
       case _ => println("その他")
     }
 
-    var s1:String = new String("ほげ")
+//    val s1:String = new String("ほげ")
     s0 match {
-      case s1 => println("hoge")
+//        到達しえない
+//      case s1 => println("hoge")
       case _ => println("その他")
     }
 
