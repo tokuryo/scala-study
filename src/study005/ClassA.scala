c6d88fe3e0e73a46c2dd1006e963a3c4f44d849e
tokuryo <tokuryo@gmail.com>
2013-09-23 22:13:11 +0900

    コメント修正

--- src/study005/ClassA.scala
+++ src/study005/ClassA.scala
@@ -1,12 +1,5 @@
 package study005
 
-/**
- * Created with IntelliJ IDEA.
- * User: tokuryo
- * Date: 13/09/12
- * Time: 22:52
- * To change this template use File | Settings | File Templates.
- */
 @cloneable
 class ClassA {
   var varInA: String = null
