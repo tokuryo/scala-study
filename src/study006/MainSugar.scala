aa253117410a1499bd8ec7a99514211f5c2fe3c9
tokuryo <tokuryo@gmail.com>
2013-10-11 17:29:30 +0900

    apply, update

--- src/study006/MainSugar.scala
+++ src/study006/MainSugar.scala
@@ -7,5 +7,14 @@ object MainSugar {
     val ten = 10
     val val010 = one.to(ten)
     val val011 = one to ten
+
+    val array = Array(1, 2, 3, 4)
+    // applyを省略できる。下記の２行は同じ意味。
+    println(array.apply(0))
+    println(array(0))
+
+    // 代入の場合、updateを省略できる。下記の２行は同じ意味。
+    array.update(0, 10)
+    array(0) = 10
   }
 }
