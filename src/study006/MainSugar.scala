61c80ceadeb471c753768c5fccea8c21af239754
tokuryo <tokuryo@gmail.com>
2013-10-29 11:51:25 +0900

    リファクタリング

--- src/study006/MainSugar.scala
+++ src/study006/MainSugar.scala
@@ -5,16 +5,19 @@ object MainSugar {
     // メソッドの引数が１つだけなら、ドットと括弧を省略できる。
     val one = 1
     val ten = 10
-    val val010 = one.to(ten)
-    val val011 = one to ten
+    val range01 = one.to(ten)
+    println("range01 : " + range01)
+    val range02 = one to ten
+    println("range02 : " + range02)
 
     val array = Array(1, 2, 3, 4)
-    // applyを省略できる。下記の２行は同じ意味。
-    println(array.apply(0))
-    println(array(0))
+    // applyメソッドの呼び出しは、"apply"の記述を省略できる。下記の２行は同じ意味。
+    println("array.apply(0) : " + array.apply(0))
+    println("array(0) : " + array(0))
 
-    // 代入の場合、updateを省略できる。下記の２行は同じ意味。
+    // updateメソッドの代わりに = で代入できる。下記の２行は同じ意味。
     array.update(0, 10)
     array(0) = 10
+    println("代入後 array(0) : " + array(0))
   }
 }
