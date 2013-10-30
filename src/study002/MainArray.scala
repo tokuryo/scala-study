a92590b5a10296b5b1c6ef86f6240fd764d4de68
tokuryo <tokuryo@gmail.com>
2013-10-30 17:12:16 +0900

    コメントの追加等。

--- src/study002/MainArray.scala
+++ src/study002/MainArray.scala
@@ -1,21 +1,26 @@
 package study002
 
+// Javaの配列相当。
 object MainArray {
 
   def main(args: Array[String]): Unit = {
     println("ArraySample")
     val a1 = new Array[Int](3)
-    println("a1.length:" + a1.length)
-    println(a1(0))
-    println(a1(1))
-    println(a1(2))
+    println("a1.length : " + a1.length)
+    println("a1(0) : " + a1(0))
+    println("a1(1) : " + a1(1))
+    println("a1(2) : " + a1(2))
+    // ミュータブル。但し長さは変更できない。
     a1(0) = 100
-    println(a1(0))
+    println("a1(0)代入後 : " + a1(0))
 
-    val a2 = Array(1, 2, 3, 4)
-    println("a2.length" + a2.length)
-    println(a2(0))
-    println(a2(3))
-
+    println()
+    // 初期値を与える例
+    val a2 = Array(2, 3, 9, 7)
+    println("a2.length : " + a2.length)
+    println("a2(0) : " + a2(0))
+    println("a2(1) : " + a2(1))
+    println("a2(2) : " + a2(2))
+    println("a2(3) : " + a2(3))
   }
 }
